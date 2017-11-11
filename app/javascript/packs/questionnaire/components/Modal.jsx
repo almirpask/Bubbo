7922d03dd3925b077d048d833f2c76f2c485246b
Almir  Santos Filho <almir@MacBook-Pro-de-Almir.local>
2017-11-11 18:18:06 -0200

    added description and number to questions modal

--- app/javascript/packs/questionnaire/components/Modal.jsx
+++ app/javascript/packs/questionnaire/components/Modal.jsx
@@ -1,20 +1,67 @@
 import React, {Component} from 'react'
 
 export default class Modal extends Component{
+    constructor(props){
+        super(props)
+        this.state= {
+            questionnaire_id: $('#questionnaire_id').val(),
+            number: 0,
+            description: ''
+        }
+        this.changeDescription = this.changeDescription.bind(this)
+        this.changeNumber = this.changeNumber.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    changeDescription(event){
+        console.log(event.target.value)
+        this.setState({description: event.target.value})
+    }
+    changeNumber(event){
+        console.log(event.target.value)
+        this.setState({number: event.target.value});
+    }
+
+    handleSubmit(){
+
+        let question = {
+            questionnaire_id: this.state.questionnaire_id,
+            number: this.state.number,
+            description: this.state.description
+        }
+        //alert(question)
+        $.post('/api/v1/questions', {question}, data => {console.log(data)})
+
+    }
+ 
     componentDidMount(){
         $('.modal').modal();
     }
     render(){
+        
         return(
             <div>
                 <div id="modal1" className="modal">
                     <div className="modal-content">
-                        <h4>Modal Header</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        <h4>Pergunta</h4>
+                        <div className="row border">
+                            <div className="col s12">
+                                <div className="input-field col s4">
+                                    <input type="number" id="number" value={this.state.number} onChange={this.changeNumber}/>
+                                    <label htmlFor="number">Numero da pergunta</label>
+                                </div>
+                                <div className="input-field col s8">
+                                    <input type="text" id="description" value={this.state.description} onChange={this.changeDescription}/>
+                                    <label htmlFor="description">Descrição</label>   
+                                </div>
+                                
+                                
+                            </div>
+                        </div>
                     </div>
                     <div className="modal-footer">
-                        <a href="#!" className="modal-action modal-close waves-effect waves-red btn-flat ">Disagree</a>
-                        <a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat ">Agree</a>
+                        <a href="#!" className="modal-action modal-close waves-effect waves-red btn-flat yellow-text text-darken-4">Fechar</a>
+                        <a className="modal-action modal-close waves-effect waves-green btn-flat green-text" onClick={this.handleSubmit}>Salvar</a>
                     </div>
                 </div>
             </div>
