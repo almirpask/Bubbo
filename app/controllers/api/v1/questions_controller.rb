8b8c0a1300f158691e5f3d8286a4ab8ff658a0bd
Almir  Santos Filho <almir@MacBook-Pro-de-Almir.local>
2017-11-12 17:22:24 -0200

    add questionnaire method

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -12,6 +12,16 @@ class Api::V1::QuestionsController < BackofficeController
     end
 
 
+    def questionnaire
+        @id = params.permit(:id)
+        @questions = Question.where(:questionnaire_id => @id[:id])
+        
+        respond_to do |format|
+            format.json { render json: @questions}
+        end
+    end
+
+
     private 
 
     def question_params
