767bbc2a211f1168d3c22c6af6477ec499edb920
Almir  Santos Filho <almir@MacBook-Pro-de-Almir.local>
2017-11-12 17:25:48 -0200

    added the refresh method

--- app/javascript/packs/questionnaire/components/Application.jsx
+++ app/javascript/packs/questionnaire/components/Application.jsx
@@ -4,38 +4,51 @@
 
 import React, {Component} from 'react'
 import List from './List'
-import Modal from './Modal'
+import Question from './Question'
 
 
 export default class Application extends Component{
-  constructor(props){
-    super(props)
+	constructor(props){
+		super(props)
+		this.state = {
+			questions: [],
+			questionnaire_id: $('#questionnaire_id').val()
+		}
+		this.refresh()
+		this.showQuestions = this.showQuestions.bind(this)
+		this.refresh = this.refresh.bind(this)
+	}
+	openModal(){
+		$('#modal1').modal('open');
+	}
+	refresh(){
+		$.get(`/api/v1/questions/questionnaire/${this.state.questionnaire_id}.json`,{},(data) => {
+			this.setState({questions: data}),
+			console.log('app'+data)
+		})
+	}
 
-    this.state = {questions: []}
-  }
-  openModal(){
-    $('#modal1').modal('open');
-  }
-
-  componentDidMount() {
-    
-  }
   
+	showQuestions(){
+		let questions = this.state.questions.concat(this.state.questions)
+		this.setState({questions: questions})
+		//this.setState({questions: questions})
+	}
 
-  render(){
-    return (
-      <div className="row">
-        <div className="col s12">
-          <h4 className="header2">Perguntas</h4>
-            <div className="row">
-                <button type="submit" className="btn btn-waves  right green" onClick={this.openModal} href="#modal1">Adicionar</button>
-            </div>
-            <Modal/>
-            <List questions={this.state.questions}/>
-        </div>
-      </div>
-    )
-  }
+	render(){
+		return (
+		<div className="row">
+			<div className="col s12">
+			<h4 className="header2" onClick={this.showQuestions}>Perguntas</h4>
+				<div className="row">
+					<button type="submit" className="btn btn-waves  right green" onClick={this.openModal} href="#modal1">Adicionar</button>
+				</div>
+				<Question refresh={this.refresh}/>
+				<List questions={this.state.questions}/>
+			</div>
+		</div>
+		)
+	}
 }
 
 
--- app/javascript/packs/questionnaire/components/List.jsx
+++ app/javascript/packs/questionnaire/components/List.jsx
@@ -3,32 +3,67 @@ import React, {Component} from 'react'
 
 
 export default class List extends Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			questions: [],
+			questionnaire_id: $('#questionnaire_id').val()
+		}
+		this.renderRows = this.renderRows.bind(this)
+		this.renderRows2 = this.renderRows2.bind(this)
+		this.refresh()
+	}
+	
+	renderRows(){
+		
+		
+		return this.state.questions.map(list => (
+			
+			<tr key={list.id}>
+				<td>{list.number}</td>
+				<td>{list.description}</td>
+				<td><i className="material-icons">edit</i></td>
+			</tr>		
+		))
+	}
+	renderRows2(){
+		let list = this.props.questions || []
+		
+		return list.map(list => (
+			
+			<tr key={list.id}>
+				<td>{list.number}</td>
+				<td>{list.description}</td>
+				<td><i className="material-icons">edit</i></td>
+			</tr>		
+		))
+	}
+
+	refresh(){
+		$.get(`/api/v1/questions/questionnaire/${this.state.questionnaire_id}.json`,{},(data) => {
+			this.setState({questions: data}),
+			console.log(data)
+		})
+	}
+
 	
 	render(){
-		let questions = this.props.questions.map(function(question){
-			return(
-				<div>
-					<tr>
-						<td>{question.number}</td>
-						<td>{question.description.subjstring(0, 10)}</td>
-						<td><i className="material-icons">edit</i></td>
-					</tr>
-				</div>
-			)
-		})
 		return (
-			<table className="centered striped">
-					<thead>
-						<tr>
-							<th>#</th>
-							<th>Question</th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-						{questions}
-					</tbody>
-			</table>
+			<div>
+				<table className="centered striped">
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Question</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+							
+							{this.renderRows2()}
+						</tbody>
+				</table>
+			</div>
 		)
 	}
 }
