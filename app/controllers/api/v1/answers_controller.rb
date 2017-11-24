29a31b7dbb86e6926420364be5be1e6c30c3aa12
Almir  Santos Filho <almir@MacBook-Pro-de-Almir.local>
2017-11-24 14:34:18 -0200

    add rights and wrongs in the render calls answers

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -10,64 +10,38 @@ class Api::V1::AnswersController < Api::V1::ApiController
     def create
         begin
             @answers = params_answer
-            # @answers = JSON.parse(@answers)
-            # render json: @answers
-            # @params = JSON.parse(@answers[:user_id])
-            @questions = JSON.parse(@answers[:questions])
-            # render json: @questions
-                unless @answers.empty?  
-                    # @a
+            @questions = params[:questions]
+            unless @answers.empty?
+                if Answer.where(user_id: @answers[:user_id]).where(questionnaire_id: @answers[:questionnaire_id]).empty?
                     @questions.each do |answer|
-                        # @a = answer["question_id"]
-                        # render json: @a
-                        
-                        Answer.create(user_id: JSON.parse(@answers[:user_id]), questionnaire_id: JSON.parse(@answers[:questionnaire_id]), alternative_id: answer["alternative_id"])
+                        Answer.create(user_id: @answers[:user_id], questionnaire_id: @answers[:questionnaire_id], alternative_id: answer[:alternative_id])
                     end
                     answers = Answer.where(:user_id => @answers[:user_id]).where(:questionnaire_id => @answers[:questionnaire_id])
                     @responses = Array.new;
                     answers.each do |a| 
                         alternatives = Alternative.where(:id => a.alternative_id)
                         alternatives.each do |alt|
-                            @responses << {alternative_id: a.alternative_id, answer: alt.answer}
-                            # unless alt.answer
-                            #   @responses << {alternative_id: a.alternative_id, answer: alt.answer}
-                            # else
-                            #     @responses << {alternative_id: a.alternative_id, answer: alt.answer}
-                            # end
+                            @responses << {alternative_id: a.alternative_id, answer: alt.answer} 
                         end
-                        # if r[:answer]
-                        # #     # @response << {alternative_id: a.alternative_id, answer: true}
-                        # # else
-                        # #     # @response << {alternative_id: a.alternative_id, answer: false}
-                        # end
+                        
                     end
-                    # @apps.select {|a| a.environment == 0}.count
-                    #a = @responses.select {|r| r[:answer] == false}.count
-                    a = @responses.partition { |r| r[:answer] }
-
-                    # @rights = Answer.
-                    render json: {answers: @responses, rights: a[0].count, wrongs: a[1].count}
-                    
+                    rw = @responses.partition { |r| r[:answer] }
+                    render json: {answers: @responses, rights: rw[0].count, wrongs: rw[1].count, answered: false}
                 else
-                    render json: {answers: {}, rights: 0, wrongs: 0}
+                    render json: {answers: {}, rights: 0, wrongs: 0, answered: true} 
                 end
+                
+            else
+                render json: {answers: {}, rights: 0, wrongs: 0, answered: false}
+            end
         rescue => exception
-            a = File.new("answer", "w")
-            a.write(exception.message)
-            
+            render json: {error: exception.message }
         end
-
-        # @answers = params_answer
-
-        # render json: @answers
-       
     end
 
     private
 
     def params_answer
-        params.permit(:user_id, :questions, :questionnaire_id)
+        params.permit(:user_id, :questionnaire_id, :questions)
     end
-
-    
 end
